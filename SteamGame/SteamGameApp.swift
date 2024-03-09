6112643832d50e16c39af16688f9e68295babb4d
amg-03 <91329661+amg-03@users.noreply.github.com>
2024-03-09 15:03:55 +0300

    Initial Commit

---
+++ SteamGame/SteamGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteamGameApp.swift
+//  SteamGame
+//
+//  Created by Арсений Гулевич  on 09.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SteamGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
